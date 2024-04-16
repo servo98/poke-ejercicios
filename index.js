9a7c1e97f0493d013ff4cd89d1ab5b1eed5e209f
servo pcerda <fernandonando98@live.com.mx>
2024-04-15 19:48:48 -0600

    promesas

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const testButton = document.getElementById('testButton');
+const promesasButton = document.getElementById('promesas');
+
 //Obtenemos al div con id contenedor
 const container = document.getElementById('contenedor');
 
 testButton.addEventListener('click', () => {
-  fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+  fetch('https://pokeapi.co/api/v2/pokemon/ditto1231231')
     .then((response) => {
-      //Convierte de respuesta a JSON
       return response.json();
     })
     .then((data) => {
-      //Hacer un div en forma de carta
-      //meter img, nombre y otros datos
-
-      const pokemon = new Pokemon(
-        data.sprites.other['official-artwork'].front_default,
-        data.name,
-        data.types
-      );
-
-      pokemon.print();
-
-      const pokemonDiv = pokemon.toDiv();
-
-      container.appendChild(pokemonDiv);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log('Ocurrió un error al pedir data al API', error);
     });
+
+  // const pokemon = new Pokemon(
+  //   data.sprites.other['official-artwork'].front_default,
+  //   data.name,
+  //   data.types
+  // );
+
+  // pokemon.print();
+
+  // const pokemonDiv = pokemon.toDiv();
+
+  // container.appendChild(pokemonDiv);
 });
