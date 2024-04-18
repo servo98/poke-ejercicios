c723f744e0babd421dbb809eda661a31f04db3c5
fernando <fernandonando98@live.com.mx>
2024-04-17 19:01:42 -0600

    type colors dynamic

--- Pokemon.js
+++ Pokemon.js
@@ -1,3 +1,26 @@
+const typeColors = {
+  normal: "#A8A878",
+  fighting: "#C03028",
+  flying: "#A890F0",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  rock: "#B8A038",
+  bug: "#A8B820",
+  ghost: "#705898",
+  steel: "#B8B8D0",
+  fire: "#F08030",
+  water: "#6890F0",
+  grass: "#78C850",
+  electric: "#F8D030",
+  psychic: "#F85888",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  fairy: "#EE99AC",
+  unknown: "#68A090",
+  shadow: "#705898",
+};
+
 class Pokemon {
   constructor(image, name, types) {
     this.image = image;
@@ -6,22 +29,24 @@ class Pokemon {
   }
 
   toDiv() {
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const card = document.createElement("div");
+    card.classList.add("card");
 
     //Image
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     img.src = this.image;
     card.appendChild(img);
 
     //Name
-    const name = document.createElement('p');
+    const name = document.createElement("p");
     name.textContent = this.name;
     card.appendChild(name);
 
     //Types
     const types = this.types.map((type) => {
-      const typeSpan = document.createElement('span');
+      const typeSpan = document.createElement("span");
+      typeSpan.classList.add("pokemon-type");
+      typeSpan.style.backgroundColor = typeColors[type.type.name];
       typeSpan.textContent = type.type.name;
       return typeSpan;
     });
